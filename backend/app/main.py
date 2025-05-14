0083e22492e0a072b21fc8543bd3cc6df74872dc
loic_jouhans <loic.jouhans@exail.com>
2025-05-14 16:26:44 +0200

    allowed CORS for frontend request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.api import simulator, reader
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # adapte à ton URL React
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(simulator.router, prefix='/simulate', tags=['Simulation'])
 app.include_router(reader.router, prefix='/read', tags=['Reading'])
